97071a3e59ab0a43f059d3bac9751ee265132060
Miti098 <jlopezn@ctpplatanares.ed.cr>
2025-05-30 16:08:49 -0600

    Notebook 2 módulo 2 , cotidiano 2025

---
+++ Modulo_2/2.6.9.py
@@ -0,0 +1,15 @@
+
+num1 = float( input("Ingresa un número: "))
+num2 = float( input("Ingresa otro número: "))   
+num1 + num2 
+  
+num1 / num2  
+print( "El resultado de la suma es:", num1 + num2)
+num1 - num2  
+print( "El resultado de la resta es:", num1 - num2)         
+num1 * num2 
+print( "El resultado de la multiplicación es:", num1 * num2)
+num1 / num2
+print( "El resultado de la división es:", num1 / num2)
+
+print("\n¡Eso es todo, amigos!")
